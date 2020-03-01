91cf004db682659599e3e3135166c3b1242f13db
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-01 10:41:30 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,18 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy
+  attr_accessor :name, :breed, :age
+
+  def initialize(hash)
+    hash.each do |key, value|
+      send("#{key}=", value)
+    end
+  end
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+end
+
+  
